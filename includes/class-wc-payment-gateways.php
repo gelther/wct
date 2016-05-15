343b3f31d4a56baf0abb445f88dcfc03fd6923a9
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-15 20:19:53 +0200

    Code consistency changes for includes/class-wc-payment-gateways.php

--- includes/class-wc-payment-gateways.php
+++ includes/class-wc-payment-gateways.php
@@ -146,7 +146,7 @@ class WC_Payment_Gateways {
 			if ( $gateway->is_available() ) {
 				if ( ! is_add_payment_method_page() ) {
 					$_available_gateways[ $gateway->id ] = $gateway;
-				} elseif( $gateway->supports( 'add_payment_method' ) ) {
+				} elseif ( $gateway->supports( 'add_payment_method' ) ) {
 					$_available_gateways[ $gateway->id ] = $gateway;
 				} elseif ( $gateway->supports( 'tokenization' ) ) {
 					$_available_gateways[ $gateway->id ] = $gateway;
