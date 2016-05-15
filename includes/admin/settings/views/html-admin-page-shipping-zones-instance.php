26c7b4294c9a9bdd6c1bef2474a9f5f294f05838
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-15 17:56:54 +0200

    Code consistency changes for includes/admin/settings/views/html-admin-page-shipping-zones-instance.php

--- includes/admin/settings/views/html-admin-page-shipping-zones-instance.php
+++ includes/admin/settings/views/html-admin-page-shipping-zones-instance.php
@@ -4,4 +4,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <h2><?php echo esc_html( $shipping_method->get_method_title() ); ?> <?php wc_back_link( __( 'Return to Shipping Methods', 'woocommerce' ), admin_url( 'admin.php?page=wc-settings&tab=shipping&zone_id=' . absint( $zone->get_zone_id() ) ) ); ?></h2>
-<?php $shipping_method->admin_options(); ?>
+<?php $shipping_method->admin_options();
