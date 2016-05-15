d480be031f22f29f3cbe388127f86038939da29b
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-15 17:59:25 +0200

    Code consistency changes for includes/class-wc-api.php

--- includes/class-wc-api.php
+++ includes/class-wc-api.php
@@ -67,7 +67,7 @@ class WC_API {
 	 * Add new query vars.
 	 *
 	 * @since 2.0
-	 * @param array $vars
+	 * @param  array    $vars
 	 * @return string[]
 	 */
 	public function add_query_vars( $vars ) {
@@ -83,7 +83,6 @@ class WC_API {
 	 * @since 2.0
 	 */
 	public static function add_endpoint() {
-
 		// REST API
 		add_rewrite_rule( '^wc-api/v([1-3]{1})/?$', 'index.php?wc-api-version=$matches[1]&wc-api-route=/', 'top' );
 		add_rewrite_rule( '^wc-api/v([1-3]{1})(.*)?', 'index.php?wc-api-version=$matches[1]&wc-api-route=$matches[2]', 'top' );
@@ -91,7 +90,6 @@ class WC_API {
 		// WC API for payment gateway IPNs, etc
 		add_rewrite_endpoint( 'wc-api', EP_ALL );
 	}
-
 
 	/**
 	 * Handle REST API requests.
@@ -118,7 +116,7 @@ class WC_API {
 			// legacy v1 API request
 			if ( 1 === WC_API_REQUEST_VERSION ) {
 				$this->handle_v1_rest_api_request();
-			} else if ( 2 === WC_API_REQUEST_VERSION ) {
+			} elseif ( 2 === WC_API_REQUEST_VERSION ) {
 				$this->handle_v2_rest_api_request();
 			} else {
 				$this->includes();
@@ -142,7 +140,6 @@ class WC_API {
 	 * @since 2.1
 	 */
 	public function includes() {
-
 		// API server / response handlers
 		include_once( 'api/class-wc-api-exception.php' );
 		include_once( 'api/class-wc-api-server.php' );
@@ -173,7 +170,6 @@ class WC_API {
 	 * @param WC_API_Server $server the REST server
 	 */
 	public function register_resources( $server ) {
-
 		$api_classes = apply_filters( 'woocommerce_api_classes',
 			array(
 				'WC_API_Coupons',
@@ -191,14 +187,12 @@ class WC_API {
 		}
 	}
 
-
 	/**
 	 * Handle legacy v1 REST API requests.
 	 *
 	 * @since 2.2
 	 */
 	private function handle_v1_rest_api_request() {
-
 		// include legacy required files for v1 REST API request
 		include_once( 'api/v1/class-wc-api-server.php' );
 		include_once( 'api/v1/interface-wc-api-handler.php' );
@@ -322,9 +316,10 @@ class WC_API {
 
 			// Done, clear buffer and exit
 			ob_end_clean();
-			die('-1');
+			die( '-1' );
 		}
 	}
+
 }
 
 endif;
