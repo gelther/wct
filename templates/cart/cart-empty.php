e1dd2d4a31831305eb8929187e32286ea28c8391
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-15 19:50:00 +0200

    Code consistency changes for templates/cart/cart-empty.php

--- templates/cart/cart-empty.php
+++ templates/cart/cart-empty.php
@@ -25,13 +25,13 @@ wc_print_notices();
 ?>
 
 <p class="cart-empty">
-	<?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?>
+	<?php _e( 'Your cart is currently empty.', 'woocommerce' ); ?>
 </p>
 
 <?php do_action( 'woocommerce_cart_is_empty' ); ?>
 
 <p class="return-to-shop">
 	<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-		<?php _e( 'Return To Shop', 'woocommerce' ) ?>
+		<?php _e( 'Return To Shop', 'woocommerce' ); ?>
 	</a>
 </p>
