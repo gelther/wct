5b5a71128c8c7e4a191291eccf5005f02f7aa288
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-31 21:33:28 +0200

    Code consistency changes for tests/framework/helpers/class-wc-helper-customer.php

--- tests/framework/helpers/class-wc-helper-customer.php
+++ tests/framework/helpers/class-wc-helper-customer.php
@@ -12,7 +12,6 @@ class WC_Helper_Customer {
 	 *
 	 * @return WC_Customer
 	 */
-
 	public static function create_mock_customer() {
 
 		$customer_data = array(
@@ -42,7 +41,6 @@ class WC_Helper_Customer {
 	 *
 	 * @return array
 	 */
-
 	public static function get_expected_store_location() {
 		return array( "GB", "", "", "" );
 	}
@@ -52,7 +50,6 @@ class WC_Helper_Customer {
 	 *
 	 * @return array
 	 */
-
 	public static function get_customer_details() {
 		return WC()->session->get( 'customer' );
 	}
@@ -62,7 +59,6 @@ class WC_Helper_Customer {
 	 *
 	 * @return array
 	 */
-
 	public static function get_chosen_shipping_methods() {
 		return WC()->session->get( 'chosen_shipping_methods' );
 	}
@@ -72,7 +68,6 @@ class WC_Helper_Customer {
 	 *
 	 * @return string base or billing
 	 */
-
 	public static function get_tax_based_on() {
 		return get_option( 'woocommerce_tax_based_on' );
 	}
@@ -82,7 +77,6 @@ class WC_Helper_Customer {
 	 *
 	 * @param string $default_shipping_method Shipping Method slug
 	 */
-
 	public static function set_customer_details( $customer_details ) {
 		WC()->session->set( 'customer', $customer_details );
 	}
@@ -92,7 +86,6 @@ class WC_Helper_Customer {
 	 *
 	 * @param string $chosen_shipping_method Shipping Method slug
 	 */
-
 	public static function set_chosen_shipping_methods( $chosen_shipping_methods ) {
 		WC()->session->set( 'chosen_shipping_methods', $chosen_shipping_methods );
 	}
@@ -102,7 +95,6 @@ class WC_Helper_Customer {
 	 *
 	 * @param string $default_shipping_method Shipping Method slug
 	 */
-
 	public static function set_tax_based_on( $default_shipping_method ) {
 		update_option( 'woocommerce_tax_based_on', $default_shipping_method );
 	}
